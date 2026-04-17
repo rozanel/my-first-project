d7d9d73d71a2d3bf2d5698255f8ef33ea74492e6
BawlSaks <thulvutheasak@gmail.com>
2026-04-17 22:05:02 +0700

    Refactor admin.py to use ModelAdmin classes
    Refactored admin registration for models and added StudySession admin.

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Subject, Chapter, Flashcard
+from .models import Subject, Chapter, Flashcard, StudySession
 
-admin.site.register(Subject)
-admin.site.register(Chapter)
-admin.site.register(Flashcard)
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'icon')
+    list_filter = ('user',)
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'order', 'card_count')
+    list_filter = ('subject',)
+
+@admin.register(Flashcard)
+class FlashcardAdmin(admin.ModelAdmin):
+    list_display = ('question', 'chapter')
+    list_filter = ('chapter__subject',)
+    search_fields = ('question', 'answer')
+
+@admin.register(StudySession)
+class StudySessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'chapter', 'score', 'streak_reached', 'coins_earned', 'created_at')
+    list_filter = ('user',)
